4f7ac0eeb5bfdf26216911d5ae66bd4d20bf5b00
Gabryel Januario <gabryeelcomy@gmail.com>
2025-01-05 13:49:19 -0300

    created download_route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from .upload import upload_bp
 from .convert import convert_bp
+from .download import download_bp
 
 def init_app(app: Flask):
     app.register_blueprint(upload_bp, url_prefix='/api')
     app.register_blueprint(convert_bp, url_prefix='/api')
+    app.register_blueprint(download_bp, url_prefix='/api')
---
+++ app/routes/download.py
@@ -0,0 +1,17 @@
+from flask import send_from_directory, current_app, jsonify, Blueprint
+import os
+
+
+download_bp = Blueprint('download', __name__)
+
+@download_bp.route("/download/<filename>", methods=['GET'])
+def download_file(filename):
+
+    converted_folder = current_app.config["CONVERTED_FOLDER"]
+
+    file_path = os.path.join(converted_folder, filename)
+    
+    if not os.path.exists(file_path):
+        return jsonify({"error": "File not found"}), 404
+
+    return send_from_directory(directory=converted_folder, path=filename, as_attachment=True)
