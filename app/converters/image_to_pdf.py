68ff0409f11c93b792870965ba5dcea1d98c9f36
Gabryel Januario <gabryeelcomy@gmail.com>
2025-01-21 09:22:28 -0300

    improving quality and automating guidance

--- app/converters/image_to_pdf.py
+++ app/converters/image_to_pdf.py
@@ -12,25 +12,30 @@ def image_to_pdf(input_path, output_path):
         raise ValueError("The output file must have a .pdf extension")
     
     image = Image.open(input_path)
-    
-    pdf = FPDF()
-    pdf.add_page()
+
+    if image.mode == 'RGBA':
+        image = image.convert('RGB')
 
     image_width, image_height = image.size
-    pdf_width = 210  
-    pdf_height = 297  
+    is_landscape = image_width > image_height
+    
+    pdf = FPDF(orientation="L" if is_landscape else "P", unit="mm", format=(210, 297))
+    pdf.add_page()
+
+    pdf_width = 297 if is_landscape else 210
+    pdf_height = 210 if is_landscape else 297
     
     scale = min(pdf_width / image_width, pdf_height / image_height)
 
-    new_width = image_width * scale
-    new_height = image_height * scale
+    new_width = int(image_width * scale)
+    new_height = int(image_height * scale)
     
     
     x_offset = (pdf_width - new_width) / 2
     y_offset = (pdf_height - new_height) / 2
 
     temp_image_path = "temp_image.jpg"
-    image = image.resize((int(new_width), int(new_height)), Image.LANCZOS)
+    image = image.resize((new_width, new_height), Image.LANCZOS)
     image.save(temp_image_path, format="JPEG", quality=95, optimize=True)
 
     pdf.image(temp_image_path, x=x_offset, y=y_offset, w=new_width, h=new_height)
